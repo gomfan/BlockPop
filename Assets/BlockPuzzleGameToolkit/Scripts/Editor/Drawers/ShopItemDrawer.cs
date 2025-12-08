be2ac1404ba867a0e2eb658e6c1012203396fb3a
gansik <gomfan@naver.com>
2025-12-08 15:39:04 +0900

    기본세팅
    기본세팅

---
+++ Assets/BlockPuzzleGameToolkit/Scripts/Editor/Drawers/ShopItemDrawer.cs
@@ -0,0 +1,64 @@
+using BlockPuzzleGameToolkit.Scripts.Settings;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEditor.VersionControl;
+using UnityEngine.UIElements;
+
+namespace BlockPuzzleGameToolkit.Scripts.Editor.Drawers
+{
+    [CustomPropertyDrawer(typeof(ShopItem))]
+    public class ShopItemDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var container = new VisualElement();
+            container.style.flexDirection = FlexDirection.Row;
+            container.style.alignItems = Align.Center;
+
+            var productIDProp = property.FindPropertyRelative("productID");
+            var countProp = property.FindPropertyRelative("count");
+            var prefabProp = property.FindPropertyRelative("prefab");
+
+            var productIDField = new PropertyField(productIDProp);
+            productIDField.label = "";
+            productIDField.style.flexGrow = 1;
+            productIDField.style.marginRight = 2;
+            productIDField.tooltip = "Product ID reference";
+
+            var countField = new PropertyField(countProp);
+            countField.label = "";
+            countField.style.flexGrow = 1;
+            countField.style.marginRight = 2;
+            countField.tooltip = "Coin count";
+
+            var prefabField = new PropertyField(prefabProp);
+            prefabField.label = "";
+            prefabField.style.flexGrow = 1;
+            prefabField.tooltip = "Custom prefab (optional)";
+
+            if (prefabProp.objectReferenceValue == null)
+            {
+                var settingsProp = property.serializedObject.FindProperty("defaultPrefab");
+                if (settingsProp != null && settingsProp.objectReferenceValue != null)
+                {
+                    prefabProp.objectReferenceValue = settingsProp.objectReferenceValue;
+                    property.serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+            prefabField.RegisterValueChangeCallback(evt =>
+            {
+                property.serializedObject.ApplyModifiedProperties();
+                property.serializedObject.Update();
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            });
+
+            container.Add(productIDField);
+            container.Add(countField);
+            container.Add(prefabField);
+
+            return container;
+        }
+    }
+}
